cc2a0295f92018dc711b85adc61748c28c768134
Amol Salunke <113031652+Salunke0497@users.noreply.github.com>
2023-03-24 08:06:42 +0530

    [AMOL] : Adding for count the size of addressbook

--- AddressbookQuery.sql
+++ AddressbookQuery.sql
@@ -17,25 +17,55 @@ PhoneNumber  BIGINT NOT NULL,
 Email VARCHAR (50) NOT NULL
 );
 
---SELECT QUERY
+--SELECT QUERY UC2
 SELECT *FROM Addressbook_DB
 
---insert query from UC
+--insert query from UC3
 INSERT INTO Addressbook_DB(FirstName,LastName,Address,City,State,ZipCode,PhoneNumber,Email)
 VALUES('Amol','Salunke','Location','Mumbai','Maharashtra',402209,8390266064,'amol@gmail.com'),
 ('A','Salunke','Location','Mumbai','Maharashtra',402209,8390266064,'amol@gmail.com'),
 ('Ashu','Salunke','Location','Mumbai','Maharashtra',402209,8390266064,'amol@gmail.com')
 
---Update query
+--Update query UC4
 UPDATE Addressbook_DB SET LastName = 'Salunkhe' WHERE FirstName ='A'
 UPDATE Addressbook_DB SET Address ='other location' WHERE LastName = 'Salunke'
 UPDATE Addressbook_DB SET City = 'Mumbai' WHERE Address = 'Location'
 
----DLETE Query
+---DLETE Query UC5
 DELETE FROM Addressbook_DB WHERE FirstName = 'Amol'
 
----retrive from city or state
+---retrive from city or state UC6
 SELECT * FROM Addressbook_DB WHERE City = 'Mumbai'
 SELECT * FROM Addressbook_DB WHERE State = 'Maharashtra'
 
 
+---size of AddressBook UC7
+
+SELECT COUNT (*) AS CityCount ,City FROM AddressBook_DB GROUP BY City
+SELECT COUNT (*) AS StateCount ,State FROM AddressBook_DB GROUP BY State
+
+---UC8 find first name as per the city
+SELECT * FROM AddressBook_DB WHERE City ='Mumbai' ORDER BY FirstName
+
+---UC9 
+ALTER TABLE AddressBook_DB ADD AddressBookType VARCHAR(50)
+ALTER TABLE AddressBook_DB ADD AddressBookName VARCHAR(50)
+
+UPDATE AddressBook_DB
+SET AddressBookType = 'Profession'
+WHERE City = 'Mumbai'
+
+UPDATE AddressBook_DB
+SET AddressBookType = 'Ashu'
+WHERE Address = 'other location'
+
+UPDATE AddressBook_DB
+SET AddressBookName = 'Ashu'
+WHERE City = 'Mumbai'
+
+----UC10
+SELECT AddressBookType, COUNT (AddressBookType) AS NumberOfContactPresent FROM AddressBook_DB GROUP BY AddressBookType
+
+-----insert
+INSERT INTO AddressBook_DB(FirstName,LastName,Address,City,State,ZipCode,PhoneNumber,Email,AddressBookType,AddressBookName)
+VALUES('rahul','patil','Raigad','Alibag','Maharashtra',402108,9112712717,'rahul@gmail.com','good friend','lalu')
